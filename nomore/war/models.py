20fe2f7b37e5a7e3d69cd7f02c9202cc1f877f83
Ignacio De Maio <ignaciodemaio+formus@gmail.com>
2022-03-26 13:15:47 +1300

    Allow army unit to attack army unit

--- nomore/war/models.py
+++ nomore/war/models.py
@@ -1,7 +1,7 @@
-from abc import ABC
-
+from abc import ABC, abstractmethod
 
 # Create your models here.
+from typing import Optional
 
 
 class Army:
@@ -10,8 +10,8 @@ class Army:
     @property
     def can_be_attacked(self) -> bool:
         """
-            At this moment armies cannot be attacked, independtly of how they are configured.
-            :return: whether the army can be attacked or not.
+            At this moment armies cannot be attacked_by, independently of how they are configured.
+            :return: whether the army can be attacked_by or not.
         """
         return False
 
@@ -23,7 +23,7 @@ class ArmyUnit(ABC):
             How much life will an army unit have. Once it is less than 0, the army unit is considered dead
         """
 
-        self._attacking_damage = attacking_damage
+        self._default_attacking_damage = attacking_damage
         """
             How much damage an army unit inflicts when attacking.
         """
@@ -31,40 +31,79 @@ class ArmyUnit(ABC):
     @property
     def can_be_attacked(self) -> bool:
         """
-            At this moment an army unit can always be attacked.
+            At this moment an army unit can always be attacked_by.
 
-            :return: whether the army can be attacked or not.
+            :return: whether the army can be attacked_by or not.
         """
         return True
+
+    def attacked_by(self, attacker: 'ArmyUnit'):
+        self._life_amount = self._life_amount - attacker.attacking_damage()
+
+    @property
+    def is_dead(self) -> bool:
+        return not self.is_alive
 
     @property
     def is_alive(self) -> bool:
         return self._life_amount > 0.0
 
+    @abstractmethod
+    def attacking_damage(self) -> float:
+        pass
+
 
 class Catapult(ArmyUnit):
-    _LIFE = 50
+    _LIFE = 200
     _ATTACKING_DAMAGE = 100
 
     def __init__(self):
         super().__init__(Catapult._LIFE, Catapult._ATTACKING_DAMAGE)
 
+    def attacking_damage(self) -> float:
+        return self._default_attacking_damage
+
 
 class EquippedArmyUnit(ArmyUnit):
-    pass
+
+    def __init__(self, life_amount: int, attacking_damage: int, weapon: Optional['Weapon'] = None):
+        super().__init__(life_amount, attacking_damage)
+
+        self._weapon = weapon
+
+    @property
+    def weapon(self) -> Optional['Weapon']:
+        return self._weapon
+
+    @weapon.setter
+    def weapon(self, weapon: Optional['Weapon']):
+        self._weapon = weapon
+
+    def attacking_damage(self) -> float:
+        return self._default_attacking_damage + self.weapon.attacking_damage \
+            if self._weapon else self._default_attacking_damage
 
 
 class Knight(EquippedArmyUnit):
     _LIFE = 100
     _ATTACKING_DAMAGE = 20
 
-    def __init__(self):
-        super().__init__(Knight._LIFE, Knight._ATTACKING_DAMAGE)
+    def __init__(self, weapon: Optional['Weapon'] = None):
+        super().__init__(Knight._LIFE, Knight._ATTACKING_DAMAGE, weapon)
 
 
 class Archer(EquippedArmyUnit):
     _LIFE = 50
     _ATTACKING_DAMAGE = 25
 
-    def __init__(self):
-        super().__init__(Archer._LIFE, Archer._ATTACKING_DAMAGE)
+    def __init__(self, weapon: Optional['Weapon'] = None):
+        super().__init__(Archer._LIFE, Archer._ATTACKING_DAMAGE, weapon)
+
+
+class Weapon:
+    def __init__(self, attacking_damage: float) -> None:
+        self._attacking_damage = attacking_damage
+
+    @property
+    def attacking_damage(self) -> float:
+        return self._attacking_damage
--- nomore/war/tests/simple_test.py
+++ nomore/war/tests/simple_test.py
@@ -1,9 +1,8 @@
-from typing import Type, Union
+from typing import Type, Union, Optional
 
 import pytest
 
-
-from war.models import Knight, ArmyUnit, Archer, Catapult, Army
+from war.models import Knight, Archer, Catapult, Army, Weapon
 
 
 @pytest.mark.parametrize(
@@ -13,10 +12,12 @@ from war.models import Knight, ArmyUnit, Archer, Catapult, Army
         (Archer, True),
         (Catapult, True),
     ])
-def test_an_army_unit_alive_when_created(klass: Type[Union[Knight, Archer, Catapult]], expected_is_alive: bool):
+def test_an_army_unit_is_alive_when_created(klass: Type[Union[Knight, Archer, Catapult]], expected_is_alive: bool):
     army_unit = klass()
 
     assert army_unit.is_alive == expected_is_alive
+
+
 
 
 @pytest.mark.parametrize(
@@ -31,3 +32,65 @@ def test_army_item_can_be_attacked(klass: Type[Union[Knight, Archer, Catapult, A
     army_item = klass()
 
     assert army_item.can_be_attacked == expected_can_be_attacked
+
+
+@pytest.mark.parametrize(
+    "attacker_class, weapon, target_class, expected_target_is_dead",
+    [
+        # knight attack knight
+        (Knight, None, Knight, False),
+        (Knight, Weapon(79.9), Knight, False),
+        (Knight, Weapon(80.1), Knight, True),
+
+        # knight attack archer
+        (Knight, None, Archer, False),
+        (Knight, Weapon(29.9), Archer, False),
+        (Knight, Weapon(30.1), Archer, True),
+
+        # knight attach catapult
+        (Knight, None, Catapult, False),
+        (Knight, Weapon(179.9), Catapult, False),
+        (Knight, Weapon(180.1), Catapult, True),
+
+        # archer attack archer
+        (Archer, None, Archer, False),
+        (Archer, Weapon(24.9), Archer, False),
+        (Archer, Weapon(25.1), Archer, True),
+
+        # archer attack knight
+        (Archer, None, Knight, False),
+        (Archer, Weapon(74.9), Knight, False),
+        (Archer, Weapon(75.1), Knight, True),
+
+        # knight attach catapult
+        (Archer, None, Catapult, False),
+        (Archer, Weapon(174.9), Catapult, False),
+        (Archer, Weapon(175.1), Catapult, True),
+
+        # catapult attack catapult
+        (Catapult, None, Catapult, False),
+
+        # catapult attack knight
+        (Catapult, None, Knight, False),
+
+        # catapult attack archer
+        (Catapult, None, Archer, True),
+    ])
+def test_army_unit_attack_army_unit(
+        attacker_class: Type[Union[Knight, Archer, Catapult, Army]],
+        weapon: Optional[Weapon],
+        target_class: Type[Union[Knight, Archer, Catapult, Army]],
+        expected_target_is_dead: bool):
+    attacker = _make_army_unit(attacker_class, weapon)
+    target = _make_army_unit(target_class)
+
+    target.attacked_by(attacker)
+
+    assert target.is_dead == expected_target_is_dead
+
+
+def _make_army_unit(attacker_class, weapon = None):
+    if weapon:
+        return attacker_class(weapon)
+    else:
+        return attacker_class()
